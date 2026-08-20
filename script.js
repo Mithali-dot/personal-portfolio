b028026e31d64944073146165c97aae3a1bc0f8f
mithali-dot <mithalir321@gmail.com>
2026-08-20 20:16:03 +0530

    Day 2 - Advanced CSS and Responsive Design

---
+++ script.js
@@ -0,0 +1,32 @@
+const themeToggle = document.getElementById("theme-toggle");
+
+const savedTheme = localStorage.getItem("theme");
+
+if (savedTheme === "dark") {
+    document.documentElement.setAttribute("data-theme", "dark");
+}
+
+if (themeToggle) {
+    themeToggle.addEventListener("click", function () {
+
+        const currentTheme =
+            document.documentElement.getAttribute("data-theme");
+
+        if (currentTheme === "dark") {
+
+            document.documentElement.removeAttribute("data-theme");
+
+            localStorage.setItem("theme", "light");
+
+        } else {
+
+            document.documentElement.setAttribute(
+                "data-theme",
+                "dark"
+            );
+
+            localStorage.setItem("theme", "dark");
+        }
+
+    });
+}
